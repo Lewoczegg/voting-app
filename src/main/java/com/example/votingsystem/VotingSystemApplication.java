eaef8500b4f61a88a63bb75d737202beb0b751c0
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-10-29 18:30:07 +0100

    Update CandidateRepository

--- src/main/java/com/example/votingsystem/VotingSystemApplication.java
+++ src/main/java/com/example/votingsystem/VotingSystemApplication.java
@@ -1,14 +1,11 @@
 package com.example.votingsystem;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VotingSystemApplication {
-
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
         SpringApplication.run(VotingSystemApplication.class, args);
-        System.out.println("Hello");
     }
-
 }
--- src/main/java/com/example/votingsystem/repositories/CandidateRepository.java
+++ src/main/java/com/example/votingsystem/repositories/CandidateRepository.java
@@ -1,7 +1,13 @@
 package com.example.votingsystem.repositories;
 
 import com.example.votingsystem.entities.Candidate;
+import com.example.votingsystem.entities.Constituency;
+import com.example.votingsystem.entities.PoliticalParty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+    List<Candidate> findByPoliticalParty(PoliticalParty politicalParty);
+    List<Candidate> findByConstituency(Constituency constituency);
 }
