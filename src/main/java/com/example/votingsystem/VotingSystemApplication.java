7c9e63cb886ec9a7adad431e3dc6a49791092300
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-12-14 20:14:50 +0100

    Add javafx deps and configuration

--- src/main/java/com/example/votingsystem/VotingSystemApplication.java
+++ src/main/java/com/example/votingsystem/VotingSystemApplication.java
@@ -1,27 +1,11 @@
 package com.example.votingsystem;
-import com.example.votingsystem.UI.MainWindow;
-import com.example.votingsystem.services.DatabaseInitializerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import com.example.votingsystem.GUI.ChartApplication;
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VotingSystemApplication implements CommandLineRunner {
-
-    @Autowired
-    MainWindow mainWindow;
+public class VotingSystemApplication{
     public static void main(String[] args) {
-        System.setProperty("java.awt.headless", "false");
-        SpringApplication.run(VotingSystemApplication.class, args);
-    }
-
-    @Autowired
-    private DatabaseInitializerService databaseInitializerService;
-
-    @Override
-    public void run(String... args) throws Exception {
-        // databaseInitializerService.initializeDatabase();
-        mainWindow.run();
+        Application.launch(ChartApplication.class, args);
     }
 }
