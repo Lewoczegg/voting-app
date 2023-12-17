d879863193326ee653e91752915d30d930b8f70f
Lewoczegg <mateusz.lewocz46@gmail.com>
2023-12-17 19:41:22 +0100

    Initialize database

--- src/main/java/com/example/votingsystem/VotingSystemApplication.java
+++ src/main/java/com/example/votingsystem/VotingSystemApplication.java
@@ -1,11 +1,17 @@
 package com.example.votingsystem;
 import com.example.votingsystem.GUI.VotingApplication;
+import com.example.votingsystem.services.DatabaseInitializerService;
 import javafx.application.Application;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VotingSystemApplication{
+
+    @Autowired
+    private static DatabaseInitializerService databaseInitializerService;
     public static void main(String[] args) {
+        // databaseInitializerService.initializeDatabase();
         Application.launch(VotingApplication.class, args);
     }
 }
